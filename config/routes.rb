6977d08256c9b3490d2d201e87778cfcb640088f
Md Ariful Islam <gmarifulislamarif@gmail.com>
2023-08-17 21:55:20 +0600

    user password manage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
+  get 'static_pages/home'
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+  resources :passwords, only: [:create, :edit, :new, :update], param: :password_reset_token
   resources :notifications
+  put "account", to: "users#update"
+  get "account", to: "users#edit"
+  delete "account", to: "users#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
